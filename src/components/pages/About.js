a39d37b5bc08f9584aab72a3ef475e2bd42f92e8
Kiggles <ridgewwoods@gmail.com>
2023-05-23 00:55:43 -0400

    added resume.js file and started filling out about me page

---
+++ src/components/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <div className='card-body'></div>
+      <p>
+        I am a recent graduate from the University of Pennsylvania Full Stack Coding Bootcamp. I have experience with HTML, CSS, JavaScript, jQuery, mySql, Nodejs, Jest, MongoDb, Express, and React. I have worked both individually and within unique groups to create and deploy full stack applications. 
+        <br></br>
+        Before I started to learn software development, I worked at Apple for five years working my way up to a Genius role technician. I worked daily with people from all walks of life, and learned valuable interpersonal skills that help me work with and get along with almost anyone; furthermore, I developed excellent troubleshooting skills and familiarity with computer and mobile hardware and repairs.
+        <br></br>
+        I graduated from Kalamazoo College in 2018 with a degree in Classics, after following my passion for language and fascination with the ancient world. I enjoyed the puzzle-like aspects of translating Latin and Ancient Greek poetry and found a love for communication.
+      </p>
+    </div>
+  );
+}
